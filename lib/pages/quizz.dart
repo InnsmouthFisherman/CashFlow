db5c07232a6b2526f92201342ccccbc111d88039
InnsmouthFisherman <76620415+InnsmouthFisherman@users.noreply.github.com>
2024-02-05 11:19:19 +0500

    Update quizz.dart

---
+++ lib/pages/quizz.dart
@@ -0,0 +1,29 @@
+
+ /* Padding buildInputContainer(String hintText) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+        child: Column (
+            children: [ 
+            Align(
+              alignment: const FractionalOffset(0.06, 0.0),
+              child: Text(hintText)
+            ),
+            SizedBox(
+              width: 340.0,
+              child: TextField( 
+              style: const TextStyle(color: Color(0xFF293936)),
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.amber[50],
+                contentPadding: const EdgeInsets.symmetric(horizontal: 10),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+          )
+        ]
+      )
+    );
+  }
+*/
